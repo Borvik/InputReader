ccb7928c6461d1d4e72ac02c5e487f7473e24908
Chris Kolkman <ckolkman@treorisoft.com>
2016-01-05 07:14:59 -0500

    Added hidden window class to allow raw inputs to be registered against.

---
+++ InputReader/InputReader/Native/HiddenWindow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Treorisoft.InputReader.Native
+{
+    internal class HiddenWindow : NativeWindow
+    {
+        private static HiddenWindow _instance = null;
+
+        public HiddenWindow()
+        {
+            this.CreateHandle(new CreateParams());
+        }
+
+        public IntPtr StaticHandle
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new HiddenWindow();
+                return _instance.Handle;
+            }
+        }
+    }
+}

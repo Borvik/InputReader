59e9e9c17d1570bbd720ce2701042f373f76fedf
Chris Kolkman <ckolkman@treorisoft.com>
2016-01-09 12:25:04 -0500

    Added details of a demo project.

--- InputReader/InputReaderDemo/Form1.Designer.cs
+++ InputReader/InputReaderDemo/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.cmdAction = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTestBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this.txtTestBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtLog, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.cmdAction, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(424, 296);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // cmdAction
+            // 
+            this.cmdAction.Location = new System.Drawing.Point(308, 3);
+            this.cmdAction.Name = "cmdAction";
+            this.cmdAction.Size = new System.Drawing.Size(113, 23);
+            this.cmdAction.TabIndex = 1;
+            this.cmdAction.Text = "Capture Device";
+            this.cmdAction.UseVisualStyleBackColor = true;
+            this.cmdAction.Click += new System.EventHandler(this.cmdAction_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Test Box";
+            // 
+            // txtTestBox
+            // 
+            this.txtTestBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.txtTestBox, 2);
+            this.txtTestBox.Location = new System.Drawing.Point(3, 32);
+            this.txtTestBox.Name = "txtTestBox";
+            this.txtTestBox.Size = new System.Drawing.Size(418, 20);
+            this.txtTestBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.tableLayoutPanel1.SetColumnSpan(this.label3, 2);
+            this.label3.Location = new System.Drawing.Point(3, 62);
+            this.label3.Margin = new System.Windows.Forms.Padding(3, 7, 3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(418, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Events";
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.SetColumnSpan(this.txtLog, 2);
+            this.txtLog.Location = new System.Drawing.Point(3, 78);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(418, 215);
+            this.txtLog.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(424, 296);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Input Reader Demo";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button cmdAction;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTestBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLog;
     }
 }
 
--- InputReader/InputReaderDemo/Form1.cs
+++ InputReader/InputReaderDemo/Form1.cs
@@ -7,14 +7,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Treorisoft.InputReader;
+using Treorisoft.InputReader.Devices;
 
 namespace InputReaderDemo
 {
     public partial class Form1 : Form
     {
+        BatchKeyboard scanner = null;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Log(string message)
+        {
+            txtLog.AppendText(message + Environment.NewLine);
+        }
+        private void Log(string message, params object[] args)
+        {
+            Log(string.Format(message, args));
+        }
+
+        private void cmdAction_Click(object sender, EventArgs e)
+        {
+            switch (cmdAction.Text)
+            {
+                case "Capture Device":
+                    CaptureDevice();
+                    break;
+                case "Stop Capture":
+                    StopCapture();
+                    break;
+                case "Release Device":
+                    ReleaseDevice();
+                    break;
+            }
+        }
+
+        private async void CaptureDevice()
+        {
+            try
+            {
+                cmdAction.Text = "Stop Capture";
+                IntPtr deviceId = await DeviceManager.DetectKeyboard();
+                if (deviceId == IntPtr.Zero)
+                    Log("Failed to capture device");
+                else
+                {
+                    scanner = new BatchKeyboard(deviceId);
+                    scanner.BatchReceived += Scanner_BatchReceived;
+                    DeviceManager.Listen(scanner);
+                    Log("Captured device ({0})", deviceId);
+                    cmdAction.Text = "Release Device";
+                }
+            }
+            catch (Exception ex)
+            {
+                Log("Error capturing device: " + ex.Message);
+            }
+        }
+
+        private void StopCapture()
+        {
+            DeviceManager.CancelDetectKeyboard();
+            cmdAction.Text = "Capture Device";
+        }
+
+        private void ReleaseDevice()
+        {
+            DeviceManager.Listen(null);
+            scanner.BatchReceived -= Scanner_BatchReceived;
+            scanner = null;
+            cmdAction.Text = "Capture Device";
+        }
+
+        private void Scanner_BatchReceived(object sender, string e)
+        {
+            if (txtLog.InvokeRequired)
+                txtLog.Invoke((MethodInvoker)delegate { Log("Batch Input: {0}", e); });
+            else
+                Log("Batch Input: {0}", e);
         }
     }
 }
